d9e9494305d55153f8b74898117474743a332c47
David Morrow <quickbasic44@outlook.com>
2022-06-02 16:09:55 -0400

    Add project files.

---
+++ BoxView.ColorXAML/BoxView.ColorXAML/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BoxView.ColorXAML
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //StackLayout stackLayout = new StackLayout();
+            //Label label = new Label();
+            //label.Text = "My Purple Box View C# codebehind";
+            //label.FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+            //label.BackgroundColor = Color.LightBlue;
+            //Xamarin.Forms.BoxView boxView = new Xamarin.Forms.BoxView();
+            //boxView.Color = Color.FromRgba(128, 0, 128, 255);
+            //boxView.HeightRequest = 50.0;
+            //boxView.WidthRequest = 50.0;
+            //boxView.HorizontalOptions = LayoutOptions.Center;
+            //stackLayout.Children.Add(label);
+            //stackLayout.Children.Add(boxView);
+            //Content = stackLayout;
+        }
+    }
+}
